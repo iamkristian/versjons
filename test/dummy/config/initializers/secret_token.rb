087ae553a030a73e2468a117d390e2ff62a81660
Kristian Rasmussen <kr@iamkristian.com>
2014-05-20 14:01:23 +0200

    downgrade to railstie 3.2.18

---
+++ test/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dummy::Application.config.secret_token = 'c49130e9345243a9ddcfad3a9b54e9e2530508b388938c6fd1c1e82f5672a12c024e0361b3d2f611b72f6da573cc6f1802668c55a649a8479e2582ea2ff23127'
